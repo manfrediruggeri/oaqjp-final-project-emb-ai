55784390ee21eb93b041f35b3b10e5f14c0331d4
manfrediruggeri <66901936+manfrediruggeri@users.noreply.github.com>
2026-03-02 20:04:15 +0100

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        dict = {}
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        dict['anger'] = anger_score
+        dict['disgust'] = disgust_score
+        dict['fear'] = fear_score
+        dict['joy'] = joy_score
+        dict['sadness'] = sadness_score
+        # dominant emotion
+        dominant_emotion = max(dict, key=dict.get)
+        dict['dominant_emotion'] = dominant_emotion
+    return dict
